52440ab10a9b336552a798fb2aec81927e8b9944
Hesham <heshsaker9@gmail.com>
2023-06-24 17:28:29 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:bloc/bloc.dart';
+import 'package:flutter/material.dart';
+import 'package:news_app/shared/bloc_observer.dart';
+
+void main() {
+  Bloc.observer =MyBlocObserver();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: ,
+    );
+  }
+}
+
